020ee8d1b437857f93ad437e0ae47764363b646f
Dmitriy <dmitriy7smirnov@gmail.com>
2020-02-19 02:07:39 +0300

    with Collections.groupingBy instead reduce and TreeMap instead HashMap
    Signed-off-by: Dmitriy <dmitriy7smirnov@gmail.com>

--- com/nicedev/Main.java
+++ com/nicedev/Main.java
@@ -1,29 +1,24 @@
 package com.nicedev;
 
-import java.util.*;
-import java.util.stream.Collectors;
+        import java.util.*;
+        import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         String str = "сапог сарай арбуз болт бокс биржа";
-        Map<Character, ArrayList<String>> sortedMap = Arrays.stream(str.split("\\s+"))
-            .reduce(new HashMap<Character, ArrayList<String>>(), (accMap, word) -> {
-                Character character = word.toLowerCase().charAt(0);
-                accMap.computeIfAbsent(character, key -> new ArrayList<String>());
-                accMap.get(character).add(word);
-                return accMap;
-            }, (map1, map2) -> null)
-            .entrySet().stream().filter(map3 -> map3.getValue().size() > 1)
-            .map(entry ->
+        TreeMap<Character, List<String>> sortedMap = Arrays.stream(str.split("\\s+"))
+                .collect(Collectors.groupingBy(s -> s.toLowerCase().charAt(0), Collectors.toList()))
+                .entrySet().stream().filter(map -> map.getValue().size() > 1)
+                .map(entry ->
                 {entry.getValue().sort((s1, s2) -> s1.length() == s2.length() ? s1.compareTo(s2) : s2.length() - s1.length());
-                return entry;
-            })
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                    return entry;
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, TreeMap::new));
 
         String result = sortedMap
-                            .toString()
-                            .replaceAll("^\\{}", "[")
-                            .replaceAll("}$", "]");
+                .toString()
+                .replaceAll("^\\{}", "[")
+                .replaceAll("}$", "]");
 
         System.out.println(result);
     }
